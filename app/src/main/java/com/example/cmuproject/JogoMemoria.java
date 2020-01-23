89126055043bc4fd3dd3e7aa93ee8501d39c7fa9
ANA CATARINA MOREIRA HENRIQUES <8170064@estg.ipp.pt>
2020-01-23 16:39:34 +0000

    Alteração no jogo da memoria

--- app/src/main/java/com/example/cmuproject/JogoMemoria.java
+++ app/src/main/java/com/example/cmuproject/JogoMemoria.java
@@ -33,10 +33,10 @@ public class JogoMemoria extends AppCompatActivity {
 
     Integer[] numeros={101,102,103,104,105,106,201,202,203,204,205,206};
     int carta101,carta102,carta103,carta104,carta105,carta106,carta201,carta202,carta203,carta204,carta205,carta206;
-    int a;
 
-    int firstNumber, secondNumber;
-    int clickFirst, clickSecond;
+
+    int primeiraCarta, segundaCarta;
+    int primeiraCartaSelecionada, segundaCartaSelecionada;
     int cardNumber=1;
 
     @Override
@@ -76,8 +76,8 @@ public class JogoMemoria extends AppCompatActivity {
         img1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int theCard=Integer.parseInt((String) v.getTag());
-                System.out.println("THE CARD::::: " + theCard);
+                int theCard=Integer.parseInt(v.getTag().toString());
+                System.out.println("THE CARD1::::: " + theCard);
                 mostrarCartas(img1,theCard);
             }
         });
@@ -85,80 +85,80 @@ public class JogoMemoria extends AppCompatActivity {
         img2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int theCard=Integer.parseInt((String) v.getTag());
-                System.out.println("THE CARD::::: " + theCard);
+                int theCard=Integer.parseInt(v.getTag().toString());
+                System.out.println("THE CARD2::::: " + theCard);
                 mostrarCartas(img2,theCard);
             }
         });
         img3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int theCard=Integer.parseInt((String) v.getTag());
-                System.out.println("THE CARD::::: " + theCard);
+                int theCard=Integer.parseInt(v.getTag().toString());
+                System.out.println("THE CARD3::::: " + theCard);
                 mostrarCartas(img3,theCard);
             }
         });
         img4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int theCard=Integer.parseInt((String) v.getTag());
-                System.out.println("THE CARD::::: " + theCard);
+                int theCard=Integer.parseInt(v.getTag().toString());
+                System.out.println("THE CARD4::::: " + theCard);
                 mostrarCartas(img4,theCard);
             }
         });
         img5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int theCard=Integer.parseInt((String) v.getTag());
-                System.out.println("THE CARD::::: " + theCard);
+                int theCard=Integer.parseInt(v.getTag().toString());
+                System.out.println("THE CARD5::::: " + theCard);
                 mostrarCartas(img5,theCard);
             }
         });
         img6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int theCard=Integer.parseInt((String) v.getTag());
-                System.out.println("THE CARD::::: " + theCard);
+                int theCard=Integer.parseInt(v.getTag().toString());
+                System.out.println("THE CARD6::::: " + theCard);
                 mostrarCartas(img6,theCard);
             }
         });
         img7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int theCard=Integer.parseInt((String) v.getTag());
-                System.out.println("THE CARD::::: " + theCard);
+                int theCard=Integer.parseInt(v.getTag().toString());
+                System.out.println("THE CARD7::::: " + theCard);
                 mostrarCartas(img7,theCard);
             }
         });
         img8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int theCard=Integer.parseInt((String) v.getTag());
-                System.out.println("THE CARD::::: " + theCard);
+                int theCard=Integer.parseInt( v.getTag().toString());
+                System.out.println("THE CARD8::::: " + theCard);
                 mostrarCartas(img8,theCard);
             }
         });
         img9.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int theCard=Integer.parseInt((String) v.getTag());
-                System.out.println("THE CARD::::: " + theCard);
+                int theCard=Integer.parseInt(v.getTag().toString());
+                System.out.println("THE CARD9::::: " + theCard);
                 mostrarCartas(img9,theCard);
             }
         });
         img10.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int theCard=Integer.parseInt((String) v.getTag());
-                System.out.println("THE CARD::::: " + theCard);
+                int theCard=Integer.parseInt(v.getTag().toString());
+                System.out.println("THE CARD10::::: " + theCard);
                 mostrarCartas(img10,theCard);
             }
         });
         img11.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int theCard=Integer.parseInt((String) v.getTag());
-                System.out.println("THE CARD::::: " + theCard);
+                int theCard=Integer.parseInt(v.getTag().toString());
+                System.out.println("THE CARD11::::: " + theCard);
                 mostrarCartas(img11,theCard);
             }
         });
@@ -166,8 +166,9 @@ public class JogoMemoria extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 int theCard=Integer.parseInt((String) v.getTag());
-                System.out.println("THE CARD::::: " + theCard);
+                System.out.println("THE CARD12::::: " + theCard);
                 mostrarCartas(img12,theCard);
+
             }
         });
 
@@ -190,33 +191,191 @@ public class JogoMemoria extends AppCompatActivity {
         carta206=R.drawable.img206;
     }
 
-    private void mostrarCartas(ImageView iv, int card){
+    private void mostrarCartas(ImageView image, int card) {
         //coloca a imagem correta nas cartas
-        if(numeros[card]==101){
-            iv.setImageResource(carta101);
-        }else if(numeros[card]==102) {
-            iv.setImageResource(carta102);
-        }else if(numeros[card]==103) {
-            iv.setImageResource(carta103);
-        }else if(numeros[card]==104) {
-            iv.setImageResource(carta104);
-        }else if(numeros[card]==105) {
-            iv.setImageResource(carta105);
-        }else if(numeros[card]==106) {
-            iv.setImageResource(carta106);
-        }else if(numeros[card]==201) {
-            iv.setImageResource(carta201);
-        }else if(numeros[card]==202) {
-                iv.setImageResource(carta202);
-        }else if(numeros[card]==203) {
-            iv.setImageResource(carta203);
-        }else if(numeros[card]==204) {
-            iv.setImageResource(carta204);
-        }else if(numeros[card]==205) {
-            iv.setImageResource(carta205);
-        }else if(numeros[card]==206) {
-            iv.setImageResource(carta206);
+        if (numeros[card] == 101) {
+            image.setImageResource(carta101);
+            System.out.println(numeros[card] + " entrou");
+        } else if (numeros[card] == 102) {
+            image.setImageResource(carta102);
+            System.out.println(numeros[card] + " entrou");
+        } else if (numeros[card] == 103) {
+            image.setImageResource(carta103);
+            System.out.println(numeros[card] + " entrou");
+        } else if (numeros[card] == 104) {
+            image.setImageResource(carta104);
+            System.out.println(numeros[card] + " entrou");
+
+        } else if (numeros[card] == 105) {
+            image.setImageResource(carta105);
+            System.out.println(numeros[card] + " entrou");
+        } else if (numeros[card] == 106) {
+            image.setImageResource(carta106);
+            System.out.println(numeros[card] + " entrou");
+        } else if (numeros[card] == 201) {
+            image.setImageResource(carta201);
+            System.out.println(numeros[card] + " entrou");
+        } else if (numeros[card] == 202) {
+            image.setImageResource(carta202);
+            System.out.println(numeros[card] + " entrou");
+        } else if (numeros[card] == 203) {
+            image.setImageResource(carta203);
+            System.out.println(numeros[card] + " entrou");
+        } else if (numeros[card] == 204) {
+            image.setImageResource(carta204);
+            System.out.println(numeros[card] + " entrou");
+        } else if (numeros[card] == 205) {
+            image.setImageResource(carta205);
+            System.out.println(numeros[card] + " entrou");
+        } else if (numeros[card] == 206) {
+            image.setImageResource(carta206);
+            System.out.println(numeros[card] + " entrou");
         }
+        System.out.println(numeros[card]);
+        if (cardNumber == 1) {
+            if (numeros[card] > 200) {
+                primeiraCarta = numeros[card] - 100;
+            } else {
+                primeiraCarta = numeros[card];
+            }
+            cardNumber = 2;
+            primeiraCartaSelecionada = card;
+        } else if (cardNumber == 2) {
+            if (numeros[card] > 200) {
+                segundaCarta = numeros[card] - 100;
+            } else {
+                segundaCarta = numeros[card];
+            }
+            segundaCartaSelecionada = card;
+
+
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    //verifica
+                    verificarCartas();
+                }
+            }, 500);
+
+        }
+
     }
+
+    private void verificarCartas(){
+
+
+        if(primeiraCarta==segundaCarta){
+            if(primeiraCartaSelecionada==0){
+                img1.setVisibility(View.INVISIBLE);
+            }else if(primeiraCartaSelecionada==1){
+                img2.setVisibility(View.INVISIBLE);
+            }else if(primeiraCartaSelecionada==2){
+                img3.setVisibility(View.INVISIBLE);
+            }else if(primeiraCartaSelecionada==3){
+                img4.setVisibility(View.INVISIBLE);
+            }else if(primeiraCartaSelecionada==4){
+                img5.setVisibility(View.INVISIBLE);
+            }else if(primeiraCartaSelecionada==5){
+                img6.setVisibility(View.INVISIBLE);
+            }else if(primeiraCartaSelecionada==6){
+                img7.setVisibility(View.INVISIBLE);
+            }else if(primeiraCartaSelecionada==7){
+                img8.setVisibility(View.INVISIBLE);
+            }else if(primeiraCartaSelecionada==8){
+                img9.setVisibility(View.INVISIBLE);
+            }else if(primeiraCartaSelecionada==9){
+                img10.setVisibility(View.INVISIBLE);
+            }else if(primeiraCartaSelecionada==10){
+                img11.setVisibility(View.INVISIBLE);
+            }else if(primeiraCartaSelecionada==11){
+                img12.setVisibility(View.INVISIBLE);
+            }
+
+            if(segundaCartaSelecionada==0){
+                img1.setVisibility(View.INVISIBLE);
+            }else if(segundaCartaSelecionada==1){
+                img2.setVisibility(View.INVISIBLE);
+            }else if(segundaCartaSelecionada==2){
+                img3.setVisibility(View.INVISIBLE);
+            }else if(segundaCartaSelecionada==3){
+                img4.setVisibility(View.INVISIBLE);
+            }else if(segundaCartaSelecionada==4){
+                img5.setVisibility(View.INVISIBLE);
+            }else if(segundaCartaSelecionada==5){
+                img6.setVisibility(View.INVISIBLE);
+            }else if(segundaCartaSelecionada==6){
+                img7.setVisibility(View.INVISIBLE);
+            }else if(segundaCartaSelecionada==7){
+                img8.setVisibility(View.INVISIBLE);
+            }else if(segundaCartaSelecionada==8){
+                img9.setVisibility(View.INVISIBLE);
+            }else if(segundaCartaSelecionada==9){
+                img10.setVisibility(View.INVISIBLE);
+            }else if(segundaCartaSelecionada==10){
+                img11.setVisibility(View.INVISIBLE);
+            }else if(primeiraCartaSelecionada==11){
+                img12.setVisibility(View.INVISIBLE);
+            }
+
+
+        }else{
+            if(primeiraCartaSelecionada==0){
+                img1.setImageResource(R.drawable.download);
+            }else if(primeiraCartaSelecionada==1){
+                img2.setImageResource(R.drawable.download);
+            }else if(primeiraCartaSelecionada==2){
+                img3.setImageResource(R.drawable.download);
+            }else if(primeiraCartaSelecionada==3){
+                img4.setImageResource(R.drawable.download);
+            }else if(primeiraCartaSelecionada==4){
+                img5.setImageResource(R.drawable.download);
+            }else if(primeiraCartaSelecionada==5){
+                img6.setImageResource(R.drawable.download);
+            }else if(primeiraCartaSelecionada==6){
+                img7.setImageResource(R.drawable.download);
+            }else if(primeiraCartaSelecionada==7){
+                img8.setImageResource(R.drawable.download);
+            }else if(primeiraCartaSelecionada==8){
+                img9.setImageResource(R.drawable.download);
+            }else if (primeiraCartaSelecionada==9){
+                img10.setImageResource(R.drawable.download);
+            }else if(primeiraCartaSelecionada==10){
+                img11.setImageResource(R.drawable.download);
+            }else if(primeiraCartaSelecionada==11){
+                img12.setImageResource(R.drawable.download);
+            }
+
+            if(segundaCartaSelecionada==0){
+                img1.setImageResource(R.drawable.download);
+            }else if(segundaCartaSelecionada==1){
+                img2.setImageResource(R.drawable.download);
+            }else if(segundaCartaSelecionada==2){
+                img3.setImageResource(R.drawable.download);
+            }else if(segundaCartaSelecionada==3){
+                img4.setImageResource(R.drawable.download);
+            }else if(segundaCartaSelecionada==4){
+                img5.setImageResource(R.drawable.download);
+            }else if(segundaCartaSelecionada==5){
+                img6.setImageResource(R.drawable.download);
+            }else if(segundaCartaSelecionada==6){
+                img7.setImageResource(R.drawable.download);
+            }else if(segundaCartaSelecionada==7){
+                img8.setImageResource(R.drawable.download);
+            }else if(segundaCartaSelecionada==8){
+                img9.setImageResource(R.drawable.download);
+            }else if(segundaCartaSelecionada==9){
+                img10.setImageResource(R.drawable.download);
+            }else if(segundaCartaSelecionada==10){
+                img11.setImageResource(R.drawable.download);
+            }else if(segundaCartaSelecionada==10){
+                img12.setImageResource(R.drawable.download);
+            }
+        }
+        cardNumber=1;
+        primeiraCarta=-1;
+        segundaCarta=-1;
+    }
+
 
 }
